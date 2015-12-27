8e971f30a40937b4bb963c9f404509fe2434fad6
Orion Letizi <oletizi@mac.com>
2015-12-26 16:46:47 -0800

    Round-trip load, edit and save seems to work.

--- src/main/java/com/orionletizi/audiogen/ui/controller/SongEditorController.java
+++ src/main/java/com/orionletizi/audiogen/ui/controller/SongEditorController.java
@@ -1,12 +1,16 @@
 package com.orionletizi.audiogen.ui.controller;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.orionletizi.audiogen.samplersong.domain.InstrumentPattern;
 import com.orionletizi.audiogen.ui.SongEditor;
 import com.orionletizi.audiogen.ui.proxy.AccordionProxy;
 import com.orionletizi.audiogen.ui.proxy.FileChooserProxy;
 import com.orionletizi.audiogen.ui.proxy.TextFieldProxy;
-import com.orionletizi.audiogen.ui.view.InstrumentPatternPane;
+import com.orionletizi.audiogen.ui.view.InstrumentPatternEditor;
 import com.orionletizi.music.theory.ChordStructure;
 import com.orionletizi.music.theory.TimeSignature;
 import com.orionletizi.sampler.sfz.Region;
@@ -15,7 +19,6 @@ import com.orionletizi.sampler.sfz.SfzParserException;
 import com.orionletizi.sampler.sfz.SfzSamplerProgram;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -28,13 +31,22 @@ import javafx.stage.Window;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class SongEditorController implements Initializable {
+
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  static {
+    mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+  }
 
   private Window window;
   private Executor exec;
@@ -53,10 +65,12 @@ public class SongEditorController implements Initializable {
   @FXML
   private VBox keyDetail;
 
-  private FileChooserProxy fileChooserProxy;
-  private Map<Integer, InstrumentPattern> instrumentPatterns = new HashMap<>();
+  @FXML
+  private Button saveInstrumentPatternButton;
 
-  private final ObjectMapper mapper = new ObjectMapper();
+  private FileChooserProxy fileChooserProxy;
+  private Map<Integer, InstrumentPattern> instrumentPatterns = new TreeMap<>();
+
 
   public SongEditorController() {
     // this is the real constructor used by JavaFX
@@ -81,12 +95,20 @@ public class SongEditorController implements Initializable {
       setInstrumentProgram(SongEditor.programFile);
     }
     fileChooserProxy = new FileChooserProxy(window);
-    chooseInstrumentPath.setOnAction((ActionEvent event) -> {
+    chooseInstrumentPath.setOnAction((event) -> {
       info("Pressed choose instrument button!");
       chooseInstrument();
     });
     instrumentPathProxy = new TextFieldProxy(instrumentPath);
-    //keyStackProxy = new AccordionProxy(keyStack);
+
+    saveInstrumentPatternButton.setOnAction(event -> {
+      info("Saving instrument pattern to file...");
+      try {
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(instrumentProgram.getParentFile(), "attributes.json"), instrumentPatterns);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
   }
 
 
@@ -110,39 +132,40 @@ public class SongEditorController implements Initializable {
       final SfzSamplerProgram program = new SfzSamplerProgram(new SfzParser(), programResource, samples);
 
       if (attributes.isFile()) {
-        final List<InstrumentPattern> patterns = mapper.readValue(attributes, List.class);
         instrumentPatterns.clear();
-        for (final InstrumentPattern pattern : patterns) {
-          info("pattern: " + pattern);
-          instrumentPatterns.put(pattern.getSamplerNote(), pattern);
-        }
-      }
 
+        final TypeFactory typeFactory = mapper.getTypeFactory();
+        final MapType mapType = typeFactory.constructMapType(Map.class, Integer.class, InstrumentPattern.class);
+        instrumentPatterns.putAll(mapper.readValue(attributes, mapType));
+      }
       Platform.runLater(() -> {
         if (instrumentProgram != null) {
           instrumentPathProxy.setText(instrumentProgram.getAbsolutePath());
         }
+        boolean initialEditorActivated = false;
         for (int i = 0; i < 128; i++) {
+          // XXX: It's probably better to get the key set or entry set rather than
+          // iterate through all possible keys.
           final Set<Region> regions = program.getRegionsByKey((byte) i);
           if (regions != null) {
-            //keyStackProxy.addTitledPane("Key " + i, new InstrumentKeyPane());
             final int key = i;
-            final InstrumentPattern instrumentPattern = instrumentPatterns.get(i);
+            if (!instrumentPatterns.containsKey(key)) {
+              instrumentPatterns.put(key, new InstrumentPattern(new TimeSignature(4, 4), 0.25f, key, new ChordStructure()));
+            }
+
             final Label keyLabel = new Label("Key: " + i);
             keyLabel.setOnMouseEntered(event -> keyLabel.setUnderline(true));
             keyLabel.setOnMouseExited(event -> keyLabel.setUnderline(false));
             keyLabel.setOnMouseClicked(event -> {
-              InstrumentPattern patt = instrumentPattern;
-              if (patt == null) {
-                patt = new InstrumentPattern(new TimeSignature(4, 4), 0.25f, key, new ChordStructure());
-              }
-              instrumentPatterns.put(key, patt);
-              final InstrumentPatternPane instrumentPatternPane = new InstrumentPatternPane(patt);
-              final ObservableList<Node> detail = this.keyDetail.getChildren();
-              detail.clear();
-              detail.add(instrumentPatternPane);
+              activateInstrumentPatternEditor(key);
             });
             keyStack.getChildren().add(keyLabel);
+
+            if (!initialEditorActivated) {
+              // activate the instrument pattern editor for the first defined key
+              activateInstrumentPatternEditor(i);
+              initialEditorActivated = true;
+            }
           }
         }
       });
@@ -151,6 +174,19 @@ public class SongEditorController implements Initializable {
     } catch (SfzParserException e) {
       e.printStackTrace();
     }
+  }
+
+  private void activateInstrumentPatternEditor(int key) {
+    InstrumentPattern patt = instrumentPatterns.get(key);
+    info("Activating instrument pattern editor: " + key + ", pattern: " + patt);
+    if (patt == null) {
+      patt = new InstrumentPattern(new TimeSignature(4, 4), 0.25f, key, new ChordStructure());
+      instrumentPatterns.put(key, patt);
+    }
+    final InstrumentPatternEditor instrumentPatternEditor = new InstrumentPatternEditor(patt);
+    final ObservableList<Node> detail = this.keyDetail.getChildren();
+    detail.clear();
+    detail.add(instrumentPatternEditor);
   }
 
   private void info(String s) {
--- src/main/java/com/orionletizi/audiogen/ui/view/InstrumentPatternEditor.java
+++ src/main/java/com/orionletizi/audiogen/ui/view/InstrumentPatternEditor.java
@@ -2,19 +2,19 @@ package com.orionletizi.audiogen.ui.view;
 
 import com.orionletizi.audiogen.samplersong.domain.InstrumentPattern;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-public class InstrumentPatternPane extends Pane {
+public class InstrumentPatternEditor extends Pane {
   private final GridPane grid;
   private final InstrumentPattern pattern;
 
-  public InstrumentPatternPane(final InstrumentPattern pattern) {
+  public InstrumentPatternEditor(final InstrumentPattern pattern) {
     this.pattern = pattern;
     this.grid = new GridPane();
     grid.setHgap(10);
     this.getChildren().add(grid);
-    //this.getChildren().add(new Label("Note: " + pattern.getSamplerNote()));
     int row = 0;
     grid.add(new Label("Key number:"), 0, row);
     grid.add(new Label(pattern.getSamplerNote() + ""), 1, row);
@@ -25,7 +25,19 @@ public class InstrumentPatternPane extends Pane {
 
     row++;
     grid.add(new Label("Division: "), 0, row);
-    grid.add(new Label(pattern.getDivision() + ""), 1, row);
+    final TextField divisionField = new TextField(pattern.getDivision() + "");
+    divisionField.textProperty().addListener(((observable, oldValue, newValue) -> {
+      if (!"".equals(newValue)) {
+        try {
+          final float division = Float.parseFloat(newValue);
+          pattern.setDivision(division);
+        } catch (NumberFormatException e) {
+          e.printStackTrace();
+          ;
+        }
+      }
+    }));
+    grid.add(divisionField, 1, row);
 
     row++;
     grid.add(new Label("Chord Structure:"), 0, row);
